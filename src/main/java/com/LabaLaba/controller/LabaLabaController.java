9fc2b72293e5b957bfa43954a7302db427689626
Juan Christian Tjandra <juan.christian@ti.ukdw.ac.id>
2016-12-07 13:07:31 +0700

    fix supplierId -> id

--- src/main/java/com/LabaLaba/controller/LabaLabaController.java
+++ src/main/java/com/LabaLaba/controller/LabaLabaController.java
@@ -3,6 +3,7 @@ package com.LabaLaba.controller;
 import com.LabaLaba.entity.Cart;
 import com.LabaLaba.entity.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,13 +13,13 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 public class LabaLabaController {
-//	@RequestMapping("/")
-//	public String index(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
-//
-//
-//	    model.addAttribute("name", "index");
-//	    return "index";
-//	}
+	@RequestMapping("/")
+	public String index(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+
+
+	    model.addAttribute("name", "index");
+	    return "index";
+	}
 
     /*====EXPERIMENTAL ARENA ====*/
 	//sketch below(unchecked)
