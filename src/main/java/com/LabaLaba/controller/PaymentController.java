134598c248f039df11f85bf9ff57e8c9766195fb
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-01-12 14:02:04 +0700

    Payment Customer

---
+++ src/main/java/com/LabaLaba/controller/PaymentController.java
@@ -0,0 +1,7 @@
+package com.LabaLaba.controller;
+
+/**
+ * Created by rien on 1/12/17.
+ */
+public class PaymentController {
+}
---
+++ src/main/java/com/LabaLaba/repository/PaymentDetailRepository.java
@@ -0,0 +1,7 @@
+package com.LabaLaba.repository;
+
+/**
+ * Created by rien on 1/12/17.
+ */
+public class PaymentDetailRepository {
+}
